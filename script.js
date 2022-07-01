3f5b4dded19444441f89ab348ca89f93358889d7
Darima <d.dorzhieva@dunice.net>
2022-07-01 14:44:30 +0300

    Update script.js
    Added event handler, classes created

--- script.js
+++ script.js
@@ -1,13 +1,84 @@
 "use strict";
-
+const ArrowUp = "ArrowUp";
+const ArrowDown = "ArrowDown";
+const ArrowLeft = "ArrowLeft";
+const ArrowRight = "ArrowRight";
 const grid = document.querySelector(".grid");
+const score = document.querySelector(".score");
+const result = document.querySelector(".result");
+const colorCell = [
+  "#afa192",
+  "#eee4da",
+  "#ede0c8",
+  "#f2b179",
+  "#ffcea4",
+  "#e8c064",
+  "#ffab6e",
+  "#fd9982",
+  "#ead79c",
+  "#76daff",
+  "#beeaa5",
+  "#d7d4f0",
+];
 grid.innerHTML = Array(17).join('<div class="cell"></div>');
 
-// очень сложно конечно
-// - наверное можно начать с добавления слушателей на события нажатия клавиш  ← ↑ ↓ →
-// - при первом и каждом последующем нажатии создавать элемент с цифрой 2 в рандомном месте
-// - при следующем нажатии отследить какая клавиша нажата, элемент с цифрой 2
-// переместить на другую клетку в зависимости от выбранной стрелки
-// - отслеживать если два элемента пересекаются, то оставлять один элемент с суммой этих двух
-// - когда все клетки поля будут заполнены элементами закончить игру
-//
+function clickControl(event) {
+  if (event.key === ArrowLeft) {
+    console.log("ArrowLeft");
+  } else if (event.key === ArrowUp) {
+    console.log("ArrowUp");
+  } else if (event.key === ArrowRight) {
+    console.log("ArrowRight");
+  } else if (event.key === ArrowDown) {
+    console.log("ArrowDown");
+  }
+}
+
+class GameManager {
+  constructor() {
+    this.isGameOver = false;
+    this.score = 0;
+    this.board = null;
+  }
+  init() {
+    this.board = new Board();
+    document.addEventListener("keyup", clickControl);
+  }
+  checkIsGameOver() {}
+}
+class Board {
+  constructor() {
+    this.widthBoard = 4;
+    this.squares = [];
+    this.wrapper = document.querySelector(".grid");
+  }
+  init() {
+    const fragment = document.createDocumentFragment();
+    for (let index = 0; index < this.width; index++) {
+      const square = document.createElement("div");
+      square.innerHTML = "";
+      square.className = "gred-cell";
+      fragment.appendChild(square);
+      square.push(square);
+    }
+    this.wrapper.appendChild(fragment);
+  }
+  generateNewCell() {}
+  addColours() {}
+}
+class Cell {
+  constructor() {
+    this.value = "";
+    this.dom = null;
+  }
+  getValue() {
+    return this.value;
+  }
+  setValue() {}
+  getNewElement() {}
+}
+
+const start = new GameManager();
+start.init();
+
+document.addEventListener("keyup", clickControl);
